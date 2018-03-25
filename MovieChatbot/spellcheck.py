6caf6d88344bdfe518f1312b43d9dfcb303c52c0
nitinmp <nitin@capillarytech.com>
2018-03-25 11:14:59 +0530

    spellcheck done

--- MovieChatbot/spellcheck.py
+++ MovieChatbot/spellcheck.py
@@ -1,59 +1,56 @@
 import re
 from collections import Counter
+def words(text): return re.findall(r'\w+', text.lower())
 
-class spellcheck:
-    
-    def words(text): return re.findall(r'\w+', text.lower())
+WORDS = Counter(words(open('dictionary/big.txt').read()))
 
-    WORDS = Counter(words(open('C:\\Users\\garustog.ORADEV\\Desktop\\Python\\big.txt').read()))
+def P(word, N=sum(WORDS.values())): 
+	"Probability of `word`."
+	return WORDS[word] / N
 
-    def P(word, N=sum(WORDS.values())): 
-        "Probability of `word`."
-        return WORDS[word] / N
+def correction(word): 
+	#split_line(word)
+	"Most probable spelling correction for word."
+	return max(candidates(word), key=P)
 
-    def correction(word): 
-        #split_line(word)
-        "Most probable spelling correction for word."
-        return max(candidates(word), key=P)
+def candidates(word): 
+	"Generate possible spelling corrections for word."
+	return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
 
-    def candidates(word): 
-        "Generate possible spelling corrections for word."
-        return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
+def known(words): 
+	"The subset of `words` that appear in the dictionary of WORDS."
+	return set(w for w in words if w in WORDS)
 
-    def known(words): 
-        "The subset of `words` that appear in the dictionary of WORDS."
-        return set(w for w in words if w in WORDS)
+def edits1(word):
+	"All edits that are one edit away from `word`."
+	letters    = 'abcdefghijklmnopqrstuvwxyz'
+	splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
+	deletes    = [L + R[1:]               for L, R in splits if R]
+	transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
+	replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
+	inserts    = [L + c + R               for L, R in splits for c in letters]
+	return set(deletes + transposes + replaces + inserts)
 
-    def edits1(word):
-        "All edits that are one edit away from `word`."
-        letters    = 'abcdefghijklmnopqrstuvwxyz'
-        splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
-        deletes    = [L + R[1:]               for L, R in splits if R]
-        transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
-        replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
-        inserts    = [L + c + R               for L, R in splits for c in letters]
-        return set(deletes + transposes + replaces + inserts)
+def edits2(word): 
+	"All edits that are two edits away from `word`."
+	return (e2 for e1 in edits1(word) for e2 in edits1(e1))
 
-    def edits2(word): 
-        "All edits that are two edits away from `word`."
-        return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+def split_line(text):
 
-    def split_line(text):
+	# split the text
+	words = text.split()
 
-        # split the text
-        words = text.split()
+	s = []
 
-        s = []
+	# for each word in the line:
+	for word in words:
+		#x =  re.sub(r'(.)\1+', r'\1\1', word)  
+		x =  re.sub(r'(.)\[^A-Za-z0-9]+', r'\1\1', re.sub('[^A-Za-z0-9]+', '', word))          
+		#re.sub('[^A-Za-z0-9]+', '', 'Oo%^rder')
+		# print the word
+		#mylist = []
+		#mylist.append(correction(x))
+		#s.append(correction(x))
+		s.append(correction(x))
 
-        # for each word in the line:
-        for word in words:
-            #x =  re.sub(r'(.)\1+', r'\1\1', word)  
-            x =  re.sub(r'(.)\[^A-Za-z0-9]+', r'\1\1', re.sub('[^A-Za-z0-9]+', '', word))          
-            #re.sub('[^A-Za-z0-9]+', '', 'Oo%^rder')
-            # print the word
-            #mylist = []
-            #mylist.append(correction(x))
-            #s.append(correction(x))
-            s.append(correction(x))
-
-        return ' '.join(s)
+	return ' '.join(s)
